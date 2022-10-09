469ab18e2a3bdd7795b5825948b7eb26bcfaab94
Roman <stetcukroman@gmail.com>
2022-10-09 16:31:28 +0300

    admin prod search & add btn

--- src/Admin/Products/ProductsTable.jsx
+++ src/Admin/Products/ProductsTable.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { useMemo } from "react";
-import { useRef } from "react";
+// import { useMemo } from "react";
+// import { useRef } from "react";
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 import { PrettyDate } from "../../helpers";
-import { createUserAxiosRequest } from "../../requestMethods"
+import { createUserAxiosRequest, publicRequestRetail } from "../../requestMethods"
 
 
 
@@ -12,21 +12,67 @@ import { createUserAxiosRequest } from "../../requestMethods"
 export default function AdminProductsTable() {
     const [limit, setLimit ] = useState(50);
     const [page, setPage ] = useState(1);
+    const [search, setSearch] = useState('');
+    const [searchReq, setSearchReq] = useState(false);
 
     const [loadMore, setLoadMore ] = useState(false);
     const [lastPage, setLastPage ] = useState(false);
     const [products, setProducts ] = useState(null);
     
+    const [newProductLayer, setNewProductLayer] = useState(false);
 
     const [scrollY, setScrollY] = useState(window.scrollY);
     const [bodyHeight, setBodyHeight] = useState(document.body.offsetHeight);
 
     // const anchorRef = useRef();
     // const isInViewport1 = useIsInViewport(anchorRef);
-    console.log(scrollY);
+    // console.log(scrollY);
     // console.log(bodyHeight);
     
     const adminRequest = createUserAxiosRequest();
+
+
+
+    // console.log(page)
+    // console.log(loadMore)
+    // console.log(lastPage)
+
+    useEffect(()=>{
+        // console.log(!loadMore && !lastPage && products);
+        if(!loadMore && !lastPage && products){
+            setLoadMore(true);
+
+            adminRequest.get('/products/adminRequest',
+            {params:{
+                options:{
+                    page,
+                    search
+                },
+                limit
+            }}
+            )
+            .then((res)=>{
+                console.log(res.data);
+                if(res.data.length === 0){
+                    setLastPage(true);
+                    // setPage(newPage);
+                    setLoadMore(false);
+                }else{
+                    // setPage(newPage);
+                    setLoadMore(false);
+                    console.log(searchReq)
+                    if(searchReq){
+                        setProducts(res.data);
+                        setSearchReq(false);
+                    }else{
+                        setProducts([...products, ...res.data]);
+
+                    }
+                }
+            })
+            .catch(err=>console.log(err));
+        }
+    },[search, page,limit])
     
     useEffect(()=>{
         if ((window.innerHeight + scrollY) >= bodyHeight) {
@@ -40,11 +86,11 @@ export default function AdminProductsTable() {
         }
     },[scrollY])
     useEffect(()=>{
-        
+        setLoadMore(true)
         adminRequest.get('/products/adminRequest',
         {params:{
             options:{
-                page 
+                page,
             },
             limit
         }}
@@ -52,6 +98,8 @@ export default function AdminProductsTable() {
         .then((res)=>{
             console.log(res.data);
             setProducts(res.data);
+            setLoadMore(false)
+
         })
         .catch(err=>console.log(err));
 
@@ -60,51 +108,37 @@ export default function AdminProductsTable() {
             setBodyHeight(document.body.offsetHeight);
 
         };
-        const handleResize = () => {
-            setBodyHeight(document.body.offsetHeight);
-        };
 
         window.addEventListener('scroll', handleScroll);
-        // document.body.addEventListener('resize', handleResize);
         
         return () => {
             window.removeEventListener('scroll', handleScroll);
-            // document.body.removeEventListener('resize', handleResize);
         }
     }, [])
 
 
-    // useEffect(()=>{
-    //     if(products){
-    //         renderProducts();
-    //     }
-    // },[products])
-
-    
-
-
-
-    // if(!products){
-    //     return <div ref={anchorRef}>loading...</div>
-    // }
-    // console.log(products);
     return (
         <div className="all-users whiteBg">
             {
                 scrollY > 150 ? <button className="btn btn-primary btn-top" onClick={()=>window.scrollTo(0,0)}>Наверх</button> : null
             }
+            {
+                newProductLayer && <NewProductLayer setNewProductLayer={setNewProductLayer}/>
+            }
             <div className="top">
-                <h1>Користувачi </h1>
+                <h1>Товари </h1>
                 <div className="search">
                     <input type="text" id="search" 
-                        // value={search} 
-                        // onChange={changeSearchValue}
+                        value={search} 
+                        
+                        onChange={({target})=>{ setSearch(target.value); setSearchReq(true); setPage(1); setLastPage(false);}} 
                         placeholder={'Пошук (Назва)'}
                      />
                 </div>
                 <div className="btns">
                     <button className="btn btn-primary" onClick={()=>{
                         // /admin/product/:catalog/new
+                        setNewProductLayer(true);
                     }}>Додати Продукт</button>
                 </div>
             </div>
@@ -184,31 +218,10 @@ export default function AdminProductsTable() {
             return;
         }
 
-        setLoadMore(true);
-        let newPage = page + 1;
-        adminRequest.get('/products/adminRequest',
-        {params:{
-            options:{
-               page: newPage 
-            },
-            limit
-        }}
-        )
-        .then((res)=>{
-            console.log(res.data);
-            if(res.data.length === 0){
-                setLastPage(true);
-                // setPage(newPage);
-                setLoadMore(false);
-            }else{
-                setPage(newPage);
-                setLoadMore(false);
-                setProducts([...products, ...res.data]);
-            }
-            
-            
-        })
-        .catch(err=>console.log(err));
+        // let newPage = page + 1;
+        setPage((pg)=> pg + 1);
+
+        
     }
     // function renderProducts(products){
     //     if(!products || products.length === 0){
@@ -261,20 +274,6 @@ function RenderProducts({product, index, allLength}){
     }
 
         const date = PrettyDate(product.createdAt, 'dd/mm/yyyy');
-
-        // if(index === allLength - 1){
-        //     // console.log(anchorRef);
-        //     return (
-        //         <tr key={product._id} ref={anchorRef}>
-        //             <td><img src={'/src/products/' + product.image} alt={product.image} /></td>
-        //             <td>{product.title}</td>
-        //             <td>{product.price} UAH</td>
-        //             <td>{product.category ? product.category.title :  'Немає'}</td>
-        //             <td>{product?.variants.length }</td>
-        //             <td>{date}</td>
-        //         </tr>
-        //     )
-        // }
         return (
             <tr key={product._id} onClick={()=>{navigate('/admin/product/' + product._id)}}>
                 <td><img src={'/src/products/' + product.image} alt={product.image} /></td>
@@ -288,7 +287,47 @@ function RenderProducts({product, index, allLength}){
 }
 
 
+function NewProductLayer({setNewProductLayer }){
+    const [catalogs, setCatalogs] = useState([]);
+    const navigate = useNavigate();
+    useEffect(()=>{
+        getCatalogs();
 
+    },[])
+    // console.log(catalogId);
+    return(
+        <div className="import-product-layer" onClick={()=>{setNewProductLayer(false)}}>
+            <div className="import-product-layer_content" onClick={e=> e.stopPropagation()}>
+                <h2>Виберiть каталог:</h2>
+                <div className="catalogs-container">
+                    {renderCatalogs()}
+                </div>
+            </div>
+        </div>
+    )
+
+    function renderCatalogs(){
+        return catalogs.map((catalog)=>{
+            return (
+                <div className="catalog_item" key={catalog._id} onClick={()=>{
+                    navigate(`/admin/product/${catalog._id}/new`)
+                }}>{catalog.title}</div>
+            )
+        },[])
+    }
+
+    function getCatalogs() {
+        
+        publicRequestRetail.get('/catalog').then(res=>{
+            setCatalogs(res.data)
+        }).catch(err=>{ 
+            console.log(err)
+        })
+    }
+}
+
+
+ // eslint-disable-next-line
 const useIsInViewport = (element, rootMargin = 0) => {
     const [isVisible, setState] = useState(false);
 
